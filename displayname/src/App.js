f4b3968553e8ecee0baeed31159d44d09b6d8c99
yashwanth <168403870+YashwanthBLU@users.noreply.github.com>
2024-11-09 11:46:43 +0000

    Added functionality

--- displayname/src/App.js
+++ displayname/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const ValidateInput = (e) => {
+    e.preventDefault();
+    if (firstName.length > 0 && lastName.length > 0) {
+      setFullName(firstName + " " + lastName);
+      console.log(fullName);
+    } else if (firstName.length === 0) {
+      alert(`Please fill out this field`);
+    } else if (lastName.length === 0) {
+      alert(`Please fill out this field`);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Full Name Display</h1>
+      <form>
+        <label>
+          First Name:
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </label>
+      </form>
+      <button
+        type="submit"
+        onClick={(e) => {
+          ValidateInput(e);
+        }}
+      >
+        Submit
+      </button>
+      <br />
+      {fullName.length > 0 && <label>Full Name: {fullName}</label>}
     </div>
   );
 }
